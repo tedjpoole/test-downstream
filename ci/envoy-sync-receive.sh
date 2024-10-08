193f29ee035c490133a679182b5670db4622510c
Ted Poole <tpoole@redhat.com>
2024-10-08 10:35:07 +0100

    x

--- ci/envoy-sync-receive.sh
+++ ci/envoy-sync-receive.sh
@@ -66,7 +66,7 @@ gh repo set-default "${DST_REPO_PATH}"
 git config set --local merge.log 100000
 
 # Perform the merge using --no-ff option to force creating a merge commit
-if git merge --signoff --no-ff -m "${TITLE}" --log "upstream/${SRC_BRANCH_NAME}" \
+if git merge --no-ff -m "${TITLE}" --log "upstream/${SRC_BRANCH_NAME}" \
                                                         > "${SCRATCH}/mergeout"; then
     DST_NEW_HEAD_SHA="$(git rev-parse HEAD)"
     if [[ "${DST_NEW_HEAD_SHA}" != "${DST_HEAD_SHA}" ]]; then
